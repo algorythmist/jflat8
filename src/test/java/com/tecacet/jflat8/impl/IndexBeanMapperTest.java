8fb487240aaa1169b312e3cab392a5b9e88ba1ea
Dimitri Papaioannou <dimitri@trizic.com>
2017-09-25 11:12:16 -0700

    removed time-zone specific test

--- src/test/java/com/tecacet/jflat8/impl/IndexBeanMapperTest.java
+++ src/test/java/com/tecacet/jflat8/impl/IndexBeanMapperTest.java
@@ -16,7 +16,6 @@ public class IndexBeanMapperTest {
 
 		ClassicQuote quote = beanMapper
 				.apply(new ArrayRowRecord(1, new String[] { "2015-04-09", "34.52", "36.87", "1001" }));
-		assertEquals("Thu Apr 09 00:00:00 PDT 2015", quote.getDate().toString());
 		assertEquals(34.52, quote.getOpen(), 0.001);
 		assertEquals(36.87, quote.getClose(), 0.001);
 		assertEquals(1001, quote.getVolume());
@@ -30,7 +29,6 @@ public class IndexBeanMapperTest {
 
 		ClassicQuote quote = beanMapper
 				.apply(new ArrayRowRecord(1, new String[] { "35.89", "34.52", "36.87", "2015-04-09", "1001"  }));
-		assertEquals("Thu Apr 09 00:00:00 PDT 2015", quote.getDate().toString());
 		assertEquals(34.52, quote.getOpen(), 0.001);
 		assertEquals(36.87, quote.getClose(), 0.001);
 		assertEquals(1001, quote.getVolume());
