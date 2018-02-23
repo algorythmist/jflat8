924fef33082edfd30d856c67b52e132cb3a76d1c
Dimitri Papaioannou <dimitri@trizic.com>
2018-02-23 10:32:59 -0800

    generalize writer output

--- src/main/java/com/tecacet/jflat8/CSVWriter.java
+++ src/main/java/com/tecacet/jflat8/CSVWriter.java
@@ -13,58 +13,63 @@ import org.apache.commons.csv.CSVPrinter;
 
 import jodd.bean.BeanUtil;
 
-//TODO extract interface
+// TODO extract interface
 public class CSVWriter<T> {
 
-	private final BeanUtil beanUtil = BeanUtil.declaredForcedSilent;
-	private final String[] header;
-	private final String[] properties;
+    private final BeanUtil beanUtil = BeanUtil.declaredForcedSilent;
+    private final String[] header;
+    private final String[] properties;
 
-	// TODO allow class inheritance in conversion
-	private Map<Class<?>, Function<? extends Object, String>> converters = new HashMap<Class<?>, Function<?, String>>();
+    // TODO allow class inheritance in conversion
+    private Map<Class<?>, Function<? extends Object, String>> converters = new HashMap<Class<?>, Function<?, String>>();
 
-	public CSVWriter(String[] properties) {
-		this(null, properties);
-	}
+    public CSVWriter(String[] properties) {
+        this(null, properties);
+    }
 
-	public CSVWriter(String[] header, String[] properties) {
-		super();
-		this.properties = properties;
-		this.header = header;
-	}
+    public CSVWriter(String[] header, String[] properties) {
+        super();
+        this.properties = properties;
+        this.header = header;
+    }
 
-	public void writeToFile(String filename, Collection<T> beans) throws IOException {
-		FileWriter fw = new FileWriter(filename);
-		CSVPrinter csvPrinter = new CSVPrinter(fw, CSVFormat.DEFAULT);
-		if (header != null) {
-			csvPrinter.printRecord(header);
-		}
-		for (T bean : beans) {
-			String[] tokens = tokenize(bean);
-			csvPrinter.printRecord(tokens);
-		}
-		csvPrinter.close();
-		fw.close();
-	}
+    public void write(Appendable appendable, Collection<T> beans) throws IOException {
+        CSVPrinter csvPrinter = new CSVPrinter(appendable, CSVFormat.DEFAULT);
+        if (header != null) {
+            csvPrinter.printRecord(header);
+        }
+        for (T bean : beans) {
+            String[] tokens = tokenize(bean);
+            csvPrinter.printRecord(tokens);
+        }
+        csvPrinter.close();
+    }
 
-	public <C> void registerConverter(Class<C> type, Function<C, String> converter) {
-		converters.put(type, converter);
-	}
+    public void writeToFile(String filename, Collection<T> beans) throws IOException {
+        FileWriter fw = new FileWriter(filename);
+        write(fw, beans);
+        fw.close();
 
-	private String[] tokenize(T bean) {
-		return IntStream.range(0, properties.length).mapToObj(i -> beanUtil.getProperty(bean, properties[i]))
-				.map(v -> toString(v)).toArray(String[]::new);
-	}
+    }
 
-	private String toString(Object value) {
-		if (value == null) {
-			return null;
-		}
-		// TODO question marks?
-		Function converter = converters.get(value.getClass());
-		if (converter == null) {
-			return value.toString();
-		}
-		return (String) converter.apply(value);
-	}
+    public <C> void registerConverter(Class<C> type, Function<C, String> converter) {
+        converters.put(type, converter);
+    }
+
+    private String[] tokenize(T bean) {
+        return IntStream.range(0, properties.length).mapToObj(i -> beanUtil.getProperty(bean, properties[i]))
+                .map(v -> toString(v)).toArray(String[]::new);
+    }
+
+    private String toString(Object value) {
+        if (value == null) {
+            return null;
+        }
+        // TODO question marks?
+        Function converter = converters.get(value.getClass());
+        if (converter == null) {
+            return value.toString();
+        }
+        return (String) converter.apply(value);
+    }
 }
--- src/main/java/com/tecacet/jflat8/DefaultCSVWriter.java
+++ src/main/java/com/tecacet/jflat8/DefaultCSVWriter.java
@@ -1,17 +1,16 @@
 package com.tecacet.jflat8;
 
+import java.io.IOException;
+
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
-
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class DefaultCSVWriter {
 
     private final CSVPrinter csvPrinter;
 
-    public DefaultCSVWriter(FileWriter fw) throws IOException {
-        csvPrinter = new CSVPrinter(fw, CSVFormat.DEFAULT);
+    public DefaultCSVWriter(Appendable appendable) throws IOException {
+        csvPrinter = new CSVPrinter(appendable, CSVFormat.DEFAULT);
     }
 
     public void writeNext(String[] tokens) throws IOException {
