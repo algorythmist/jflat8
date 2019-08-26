f27580800a28bcc399f18ba278484b2b2fceb928
algorythmist <dimitri@tecacet.com>
2019-08-26 07:09:42 -0700

    added jacoco but no coverage checks yet

--- src/main/java/com/tecacet/jflat8/CoreFlatFileReader.java
+++ src/main/java/com/tecacet/jflat8/CoreFlatFileReader.java
@@ -7,27 +7,27 @@ import java.util.List;
 
 public interface CoreFlatFileReader<T> {
 
-	/**
-	 * Read an input stream, processing each row with a callback
-	 * 
-	 * @param is
-	 * @param callback
-	 * @throws IOException
-	 */
-	void read(InputStream is, FlatFileReaderCallback<T> callback) throws IOException;
-	
-	
-	/**
-	 * Read an input strem into a list
-	 * 
-	 * @param is
-	 * @return
-	 * @throws IOException
-	 */
-	public default List<T> readToList(InputStream is) throws IOException {
-		List<T> list = new ArrayList<>();
-		read(is, (record, bean) -> list.add(bean));
-		return list;
-	}
+    /**
+     * Read an input stream, processing each row with a callback
+     *
+     * @param is source input stream
+     * @param callback a callback for each line
+     * @throws IOException if reading fails
+     */
+    void read(InputStream is, FlatFileReaderCallback<T> callback) throws IOException;
+
+
+    /**
+     * Read an input strem into a list
+     *
+     * @param is source input stream
+     * @return the list of mapped beans
+     * @throws IOException if reading fails
+     */
+    default List<T> readToList(InputStream is) throws IOException {
+        List<T> list = new ArrayList<>();
+        read(is, (record, bean) -> list.add(bean));
+        return list;
+    }
 
 }
--- src/main/java/com/tecacet/jflat8/impl/LineParsingFlatFileReader.java
+++ src/main/java/com/tecacet/jflat8/impl/LineParsingFlatFileReader.java
@@ -5,10 +5,6 @@ import com.tecacet.jflat8.LineMapper;
 
 public class LineParsingFlatFileReader<T> extends AbstractFlatFileReader<T> {
 
-	public LineParsingFlatFileReader(LineMapper lineMapper, BeanMapper<T> beanMapper) {
-		this(lineMapper, beanMapper, new FlatFileFormat());
-	}
-	
 	public LineParsingFlatFileReader(LineMapper lineMapper, BeanMapper<T> beanMapper, FlatFileFormat fileFormat) {
 		super(new LineBasedCoreFlatFileReader<>(lineMapper, beanMapper, fileFormat));
 	}
